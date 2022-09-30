1726f884547a92d6c827170bcc312b6a79388d87
Felipe de Sousa <211061707@aluno.unb.br>
2022-09-30 12:03:55 -0300

    Mudança de nomes e algumas estilizações

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ function entrar(id){
     opcao.style.font = 'normal 20px arial'
     opcao.style.transition = 'transform 0.5s'
     opcao.style.transform = 'translateX(10px) scale(1.3)'
-    opcao.style.borderColor = 'rgb(0, 185, 185)'
+    opcao.style.backgroundColor = 'rgba(116, 36, 190, 0.78)'
 }
 function sair(id){
     let opcao = document.getElementById(id)
     opcao.style.font = 'normal 15px arial'
     opcao.style.transition = 'transform 0.5s'
     opcao.style.transform = 'translateX(0) scale(1)'
-    opcao.style.borderColor = 'transparent'
+    opcao.style.backgroundColor = 'transparent'
 }
