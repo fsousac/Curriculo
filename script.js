8677b88d9f430e7359d8a0e949af81ddcd9095a3
Felipe de Sousa <211061707@aluno.unb.br>
2022-10-02 21:12:08 -0300

    MacOS tab com imagem e mudança de fonte

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function entrar(id){
     let opcao = document.getElementById(id)
-    opcao.style.font = 'normal 20px arial'
+    opcao.style.font = 'normal 20px "Trebuchet MS"'
     opcao.style.transition = 'transform 0.5s'
     opcao.style.transform = 'translateX(10px) scale(1.3)'
     opcao.style.backgroundColor = 'rgba(116, 36, 190, 0.78)'
 }
 function sair(id){
     let opcao = document.getElementById(id)
-    opcao.style.font = 'normal 15px arial'
+    opcao.style.font = 'normal 20px "Trebuchet MS"'
     opcao.style.transition = 'transform 0.5s'
     opcao.style.transform = 'translateX(0) scale(1)'
     opcao.style.backgroundColor = 'transparent'
