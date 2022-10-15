eac76eca572f84b7d9ebbb8908b1cd26e1d19914
Felipe de Sousa <211061707@aluno.unb.br>
2022-10-14 21:33:09 -0300

    Design das opcoes e sombras da hotbar

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function entrar(id){
     let opcao = document.getElementById(id)
     opcao.style.font = 'normal 20px "Trebuchet MS"'
     opcao.style.transition = 'transform 0.5s'
-    opcao.style.transform = 'translateX(10px) scale(1.3)'
+    opcao.style.transform = 'translateX(10px) scale(1.1)'
     opcao.style.backgroundColor = 'rgba(116, 36, 190, 0.78)'
 }
 function sair(id){
